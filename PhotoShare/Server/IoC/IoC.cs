9df2339593bcccb6495d801de7f99eb60e8ad39b
Daniel Devaud <devaudan@students.zhaw.ch>
2022-06-05 15:58:04 +0200

    #4 Added File Handler

--- PhotoShare/Server/IoC/IoC.cs
+++ PhotoShare/Server/IoC/IoC.cs
@@ -1,9 +1,13 @@
-﻿namespace PhotoShare.Server.IoC
+﻿using PhotoShare.Server.Contracts;
+using PhotoShare.Server.Files;
+
+namespace PhotoShare.Server.IoC
 {
     public static class IoC
     {
         public static IServiceCollection BindServices(this IServiceCollection serviceCollection)
         {
+            serviceCollection.AddTransient<IFileHandler, FileHandler>();
 
             return serviceCollection;
         }
