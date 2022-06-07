73bfbd5eea830d4468171be71856e56ca1dd4173
Daniel Devaud <devaudan@students.zhaw.ch>
2022-06-07 19:12:07 +0200

    #6 #7 Basic CRUD Setup

---
+++ PhotoShare/Server/Controllers/GroupsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoShare.Server.Database.Context;
+using PhotoShare.Shared;
+
+namespace PhotoShare.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly PhotoShareContext _context;
+
+        public GroupsController(PhotoShareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(Guid id)
+        {
+          if (_context.Groups == null)
+          {
+              return NotFound();
+          }
+            var @group = await _context.Groups.FindAsync(id);
+
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return @group;
+        }
+
+        // PUT: api/Groups/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(Guid id, Group @group)
+        {
+            if (id != @group.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@group).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Groups
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group @group)
+        {
+          if (_context.Groups == null)
+          {
+              return Problem("Entity set 'PhotoShareContext.Groups'  is null.");
+          }
+            _context.Groups.Add(@group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGroup", new { id = @group.Id }, @group);
+        }
+
+        // DELETE: api/Groups/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroup(Guid id)
+        {
+            if (_context.Groups == null)
+            {
+                return NotFound();
+            }
+            var @group = await _context.Groups.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            _context.Groups.Remove(@group);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GroupExists(Guid id)
+        {
+            return (_context.Groups?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PhotoShare/Server/Controllers/PicturesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoShare.Server.Database.Context;
+using PhotoShare.Shared;
+
+namespace PhotoShare.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly PhotoShareContext _context;
+
+        public PicturesController(PhotoShareContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Pictures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Picture>> GetPicture(Guid id)
+        {
+          if (_context.Pictures == null)
+          {
+              return NotFound();
+          }
+            var picture = await _context.Pictures.FindAsync(id);
+
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return picture;
+        }
+
+        
+        // POST: api/Pictures
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Picture>> PostPicture(Picture picture)
+        {
+          if (_context.Pictures == null)
+          {
+              return Problem("Entity set 'PhotoShareContext.Pictures'  is null.");
+          }
+            _context.Pictures.Add(picture);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPicture", new { id = picture.Id }, picture);
+        }
+
+        // DELETE: api/Pictures/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePicture(Guid id)
+        {
+            if (_context.Pictures == null)
+            {
+                return NotFound();
+            }
+            var picture = await _context.Pictures.FindAsync(id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pictures.Remove(picture);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
