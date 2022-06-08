0450db016aa6736b456ec12a02ef00783fd78b9c
Daniel Devaud <devaudan@students.zhaw.ch>
2022-06-08 16:54:13 +0200

    #5 Basic En and decryption

---
+++ PhotoShare/Server/BusinessLogic/EncryptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using PhotoShare.Server.Contracts;
+using System.Security.Cryptography;
+
+namespace PhotoShare.Server.BusinessLogic
+{
+    public class EncryptionHandler : IEncryptionHandler
+    {
+        public Stream DecryptStream(Stream stream, Guid key)
+        {
+            using var aes = new AesManaged();
+            aes.Key = key.ToByteArray();
+            aes.IV = key.ToByteArray();
+            return new CryptoStream(stream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+        }
+
+        public Stream EncryptStream(Stream stream, Guid key)
+        {
+            using var aes = new AesManaged();
+            aes.Key = key.ToByteArray();
+            aes.IV = key.ToByteArray();
+            return new CryptoStream(stream, aes.CreateEncryptor(),CryptoStreamMode.Read);
+
+        }
+    }
+}
---
+++ PhotoShare/Server/Contracts/IEncryptionHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoShare.Server.Contracts
+{
+    public interface IEncryptionHandler
+    {
+
+        public Stream EncryptStream(Stream stream, Guid key);
+        public Stream DecryptStream(Stream stream, Guid key);
+    }
+}
